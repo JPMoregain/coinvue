9536add4eefe651a50f066b1502275262c41ae57
JP Moregain <j.p.moregain15@gmail.com>
2022-09-14 10:01:15 -0400

    Create and format table that displays coin info

--- src/components/CoinDisplay/coinDisplay.js
+++ src/components/CoinDisplay/coinDisplay.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './coinDisplay.css'
 import { LinearProgress, TableContainer, TableHead, TableRow, TextField, Table, TableCell, TableBody } from '@mui/material';
 import { Container } from '@mui/system';
+import { makeStyles } from '@mui/styles'
 
 export const CoinDisplay = () => {
   // initialize state to store coin data in an array, and boolean that tracks whether the api call has finished or not
@@ -12,7 +13,7 @@ export const CoinDisplay = () => {
   const [searchValue, setSearchValue] = useState('')
 
   // destructure props from CryptoContext to access current currency to grab API data in the correct currency
-  const { currency } = cryptoState();
+  const { currency, symbol } = cryptoState();
   const apiEndpoint = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
 
   // fetch using apiEndpoint url
@@ -62,7 +63,7 @@ export const CoinDisplay = () => {
                     color: 'black',
                   }}>
                     <TableCell style={{ border: 'none', fontWeight: '700'}}>Coin</TableCell>
-                    {['Price', '24h Change', 'Market Cap'].map((head) => (
+                    {['Ticker', 'Price', '24h Change', 'Market Cap', 'Watch'].map((head) => (
                       <TableCell
                         style={{
                           border: 'none',
@@ -76,7 +77,37 @@ export const CoinDisplay = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {handleSearch()}
+                  {/* handleSearch will return the array of filtered coins, if nothing is typed in search box it will return all of the coins */}
+                  {/* map each coin in the array onto a new row in the table */}
+                  {handleSearch().map(coin => {
+                    // determine what will be rendered for each coin in array that is returned from handleSearch()
+                    // map is overwriting each key in the array with a row and cells that will be rendered
+                    return (
+                      <TableRow key={coin.name} className='row'>
+                        <TableCell component='th' scope='row'>
+                          <div className='coinLogoName'>
+                            <img src={coin.image} alt={coin.name} height='40' style={{ marginBottom: 10 }} />
+                            <div className='coinName'>{coin.name}</div>
+                          </div>
+                        </TableCell>
+                        <TableCell align='right'>
+                          <div className='coinTicker'>{coin.symbol}</div>
+                        </TableCell>
+                        <TableCell align='right'
+                          styles={{
+                            gap: 15,
+                          }}>
+                          {symbol}{coin.current_price.toLocaleString()}
+                        </TableCell>
+                        <TableCell align='right'>
+                          {coin.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell align='right'>
+                          {symbol}{coin.market_cap.toLocaleString()}
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
                 </TableBody>
               </Table>
             )
